8044229fda9b8c65b473c214288d397ea7b50859
RyanLin5967 <idident5967@gmail.com>
2025-09-28 21:55:35 -0400

    Added todo list in README

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template
-
+import sqlite3
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
+
     return render_template("index.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
